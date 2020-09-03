c4abdb702e59619e9856fb735bd5a1462a87a346
Snezhig <SneQAstk@gmail.com>
2020-09-03 23:00:16 +0300

    Complement init script to add public dir if it is needed

--- init.sh
+++ init.sh
@@ -4,8 +4,8 @@ ENV_FILE=.env
 
 if [ -f "$ENV_FILE" ]; then
   echo "The .env file already exists."
-  read -p "Would you like to remove it? (Y|N): " -i 'N' -e remove_env_file
-  if [ "${remove_env_file^^}" == 'Y' ]; then
+  read -p "Would you like to remove it? (Y|N): " -i 'N' -e REMOVE_ENV_FILE
+  if [ "${REMOVE_ENV_FILE^^}" == 'Y' ]; then
     rm .env
     cp .env.template .env
   fi
@@ -22,4 +22,11 @@ sed -ri "s~@PATH_TO_LOGS@~$LOGS_PATH~" .env
 
 cd docker && mkdir -p "logs"
 cd "logs" && mkdir -p {"supervisor","tmp","mysql","nginx","php"}
-cd "tmp" && mkdir -p "php_sessions" && cd ../..
+cd "tmp" && mkdir -p "php_sessions" && cd ../../..
+
+if [ ! -d 'public' ]; then
+  read -p "Create public folder with index.php? " -i 'Y' -e CREATE_PUBLIC
+  if [ "${CREATE_PUBLIC^^}" == 'Y' ]; then
+    mkdir public && echo '<?php phpinfo();' >public/index.php
+  fi
+fi
