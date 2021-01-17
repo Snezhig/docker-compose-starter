fe342be861627b869d6d2e7c0c8281d1a7f92b49
snezhig <snezhig.git@gmail.com>
2021-01-17 14:11:43 +0300

    Update files

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)" || exit
+
 ENV_FILE=.env
 #Check if env file exists and offer to recreate it
 if [ -f "$ENV_FILE" ]; then
@@ -15,39 +17,49 @@ fi
 
 #Ask user to type base paths
 CURRENT_DIR=$PWD
-read -p "Type the project name: " -i "$(sed s~-~_~g <<< ${PWD##*/})" -e PROJECT_NAME
-sed -ri "s~@YOUR_PROJECT_NAME@~$PROJECT_NAME~" .env
+PARENT_DIR=$(dirname "$PWD")
 
-read -p "Type the project path: " -i "$CURRENT_DIR" -e PROJECT_PATH
+read -p "Type the project path: " -i "$PARENT_DIR" -e PROJECT_PATH
 sed -ri "s~@PATH_TO_PROJECT@~$PROJECT_PATH~" .env
 
-read -p "Type the path to docker data: " -i "$CURRENT_DIR/docker/data" -e PATH_TO_DOCKER_DATA
+PROJECT_NAME=${PROJECT_PATH%*/}
+read -p "Type the project name: " -i "$(sed s~-~_~g <<<${PROJECT_NAME##*/})" -e PROJECT_NAME
+sed -ri "s~@YOUR_PROJECT_NAME@~$PROJECT_NAME~" .env
+
+read -p "Type the domain for project: " -i "${PROJECT_NAME}.loc" -e PROJECT_DOMAIN
+sed -ri "s~@YOUR_PROJECT_DOMAIN@~$PROJECT_DOMAIN~" .env
+
+read -p "Type the path to docker data: " -i "$CURRENT_DIR/data" -e PATH_TO_DOCKER_DATA
 sed -ri "s~@PATH_TO_DOCKER_DATA@~$PATH_TO_DOCKER_DATA~" .env
 
-read -p "Type the log path: " -i "$CURRENT_DIR/docker/logs" -e LOGS_PATH
+read -p "Type the log path: " -i "$CURRENT_DIR/logs" -e LOGS_PATH
 sed -ri "s~@PATH_TO_LOGS@~$LOGS_PATH~" .env
 
-read -p "Type database name: " -e DB_NAME
+read -p "Type database name: " -i "test" -e DB_NAME
 sed -ri "s~@DB_NAME@~$DB_NAME~" .env
 
-read -p "Type database user: " -e DB_USER
+read -p "Type database user: " -i "test" -e DB_USER
 sed -ri "s~@DB_USER@~$DB_USER~" .env
 
-read -p "Type database user password: " -e DB_PASSWORD
+read -p "Type database user password: " -i "yourStrongPassword" -e DB_PASSWORD
 sed -ri "s~@DB_PASSWORD@~$DB_PASSWORD~" .env
 
-read -p "Type database root password: " -e DB_ROOT_PASSWORD
+read -p "Type database root password: " -i "yourStrongPassword" -e DB_ROOT_PASSWORD
 sed -ri "s~@DB_ROOT_PASSWORD@~$DB_ROOT_PASSWORD~" .env
 
 
-for DIRECTORY in "supervisor" "tmp" "mysql" "nginx" "php" "tmp/php_sessions"; do
+for TMP in "tmp" "tmp/php_sessions"; do
+  mkdir -p "./$TMP"
+done
+
+for DIRECTORY in "supervisor" "mysql" "nginx" "php"; do
   mkdir -p "$LOGS_PATH/$DIRECTORY"
 done
 
 #Create public directory
-if [ ! -d 'public' ]; then
+if [ ! -d "${PARENT_DIR}/public" ]; then
   read -p "Create public folder with index.php? " -i 'Y' -e CREATE_PUBLIC
   if [ "${CREATE_PUBLIC^^}" == 'Y' ]; then
-    mkdir public && echo '<?php phpinfo();' >public/index.php
+    mkdir "${PARENT_DIR}/public" && echo '<?php phpinfo();' > "${PARENT_DIR}/public/index.php"
   fi
 fi
